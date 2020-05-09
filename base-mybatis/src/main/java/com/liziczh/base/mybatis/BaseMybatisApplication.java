b89b89149832392be6755a5737a0e2f897271d9c
liziczh <liziczh@foxmail.com>
2020-05-10 01:45:45 +0800

    AOP

---
+++ base-mybatis/src/main/java/com/liziczh/base/mybatis/BaseMybatisApplication.java
@@ -0,0 +1,11 @@
+package com.liziczh.base.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseMybatisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BaseMybatisApplication.class, args);
+	}
+}
