a48211b9e8c38a7a794353a69f51639713f71ba7
liziczh <liziczh@foxmail.com>
2020-05-18 17:34:29 +0800

    service

---
+++ base-service/src/test/java/com/liziczh/base/service/BaseServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.liziczh.base.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
