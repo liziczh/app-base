93fccc8d22919cb551067cf398f971466bc875ea
liziczh <liziczh@foxmail.com>
2020-06-01 01:24:16 +0800

    SortCondition

--- base-common/src/main/java/com/liziczh/base/common/condition/BaseCondition.java
+++ base-common/src/main/java/com/liziczh/base/common/condition/BaseCondition.java
@@ -1,6 +1,7 @@
 package com.liziczh.base.common.condition;
 
 import java.io.Serializable;
+import java.util.List;
 
 import lombok.Data;
 
@@ -8,4 +9,5 @@ import lombok.Data;
 public abstract class BaseCondition implements Serializable {
 	private static final long serialVersionUID = 5582629689714183859L;
 	private PageCondition pageCondition;
+	private List<SortCondition> sortConditionList;
 }
