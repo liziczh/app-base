5980b08b9bd1661af6f0aa4341023398adbf03cd
liziczh <liziczh@foxmail.com>
2020-06-01 01:22:42 +0800

    SortCondition

---
+++ base-common/src/main/java/com/liziczh/base/common/condition/SortCondition.java
@@ -0,0 +1,33 @@
+package com.liziczh.base.common.condition;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class SortCondition implements Serializable {
+	private static final long serialVersionUID = 2685484123703694389L;
+	private String cloName;
+	private ORDER orderType;
+	/**
+	 * 排序类型
+	 */
+	public enum ORDER {
+		AEC("AEC", "升序"),
+		DESC("DESC", "降序");
+
+		private String code;
+		private String name;
+
+		private ORDER(String code, String name) {
+			this.code = code;
+			this.name = name;
+		}
+		public String getCode() {
+			return code;
+		}
+		public String getName() {
+			return name;
+		}
+	}
+}
