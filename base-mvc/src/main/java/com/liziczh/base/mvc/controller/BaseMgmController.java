acc41fda7dcf0dc5a237247a7b35c9212f56250a
zhehao.chen <zhehao.chen@ikang.com>
2020-06-01 00:58:58 +0800

    select

--- base-mvc/src/main/java/com/liziczh/base/mvc/controller/BaseMgmController.java
+++ base-mvc/src/main/java/com/liziczh/base/mvc/controller/BaseMgmController.java
@@ -8,20 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.liziczh.base.common.condition.BaseCondition;
 import com.liziczh.base.common.entity.BaseEntity;
 import com.liziczh.base.common.result.Result;
 import com.liziczh.base.common.result.ResultBuilder;
 import com.liziczh.base.common.service.BaseMgmService;
 
 @RestController
-public abstract class BaseMgmController<T extends BaseEntity, K, C> {
+public abstract class BaseMgmController<T extends BaseEntity, K, C extends BaseCondition> {
 	public abstract String getIndex() throws Exception;
 	public abstract BaseMgmService<T, K, C> getService() throws Exception;
+	@RequestMapping(value = "select", method = RequestMethod.GET)
+	public Result<List<T>> select(@RequestBody C condition) throws Exception {
+		List<T> list= getService().selectByCondition(condition);
+		return new ResultBuilder<List<T>>().complete(list);
+	}
 	@RequestMapping(value = "index", method = RequestMethod.POST)
 	public String index() throws Exception {
 		return getIndex();
 	}
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	@RequestMapping(value = "add", method = RequestMethod.POST)
 	public Result<String> add(T entity) throws Exception {
 		getService().addItem(entity);
 		return new ResultBuilder<String>().complete("操作成功");
@@ -31,12 +37,12 @@ public abstract class BaseMgmController<T extends BaseEntity, K, C> {
 		getService().updateItem(entity);
 		return new ResultBuilder<String>().complete("操作成功");
 	}
-	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "get/{id}", method = RequestMethod.GET)
 	public Result<T> getById(@PathVariable K id) throws Exception {
 		T entity = getService().get(id);
 		return new ResultBuilder<T>().complete(entity);
 	}
-	@RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+	@RequestMapping(value = "delete/{id}", method = RequestMethod.POST)
 	public Result<String> delete(@PathVariable K id) throws Exception {
 		getService().delete(id);
 		return new ResultBuilder<String>().complete("操作成功");
