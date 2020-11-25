aa2e38cc58ce27f028645c48d6daa5b491a045c8
liziczh <liziczh@foxmail.com>
2020-11-25 11:05:03 +0800

    Response

--- base-common/src/main/java/com/liziczh/base/common/response/Response.java
+++ base-common/src/main/java/com/liziczh/base/common/response/Response.java
@@ -2,8 +2,6 @@ package com.liziczh.base.common.response;
 
 import java.io.Serializable;
 
-import com.liziczh.base.common.result.ResultConstant;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
