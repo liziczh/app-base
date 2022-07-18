e01d43d23e9b80110469238f68307cadd4169f3e
chenzhehao <chenzhehao@baidu.com>
2022-07-18 15:46:23 +0800

    Mail

---
+++ base-mail/src/main/java/com/liziczh/base/mail/dto/MailSendReq.java
@@ -0,0 +1,60 @@
+package com.liziczh.base.mail.dto;
+
+import lombok.*;
+
+import java.util.Map;
+
+/**
+ * 邮件发送DTO
+ *
+ * @author chenzhehao
+ * @version 1.0
+ * @description
+ * @date 2021/8/23 9:23 下午
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MailSendReq {
+    /**
+     * 主题
+     */
+    private String subject;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 发件人
+     */
+    private String from;
+
+    /**
+     * 收件人
+     */
+    private String[] to;
+
+    /**
+     * 抄送
+     */
+    private String[] cc;
+
+    /**
+     * 密送
+     */
+    private String[] bcc;
+
+    /**
+     * 图片
+     */
+    private Map<String, String> image;
+
+    /**
+     * 附件
+     */
+    private Map<String, String> attachment;
+}
---
+++ base-mail/src/main/java/com/liziczh/base/mail/service/MailService.java
@@ -0,0 +1,27 @@
+package com.liziczh.base.mail.service;
+
+import com.liziczh.base.mail.dto.MailSendReq;
+import com.liziczh.base.mail.dto.MailSendResult;
+
+public interface MailService {
+
+    /**
+     * 发送文本邮件
+     *
+     * @param request
+     * @return com.liziczh.mvc.mesh.ref.mail.dto.MailSendResult
+     * @author chenzhehao
+     * @date 2022/1/16 7:10 下午
+     */
+    MailSendResult sendTextMail(MailSendReq request);
+
+    /**
+     * 发送MIME邮件
+     *
+     * @param request
+     * @return com.liziczh.mvc.mesh.ref.mail.dto.MailSendResult
+     * @author chenzhehao
+     * @date 2022/1/16 7:10 下午
+     */
+    MailSendResult sendMimeMail(MailSendReq request);
+}
